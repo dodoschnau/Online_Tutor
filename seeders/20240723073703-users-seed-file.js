4eff8df0c62c1eb5e6e1dd1eeb3c5db837d5649c
dodoschnau <huangmeisin@gmail.com>
2024-07-23 17:43:40 +0800

    feat: add users seed file

---
+++ seeders/20240723073703-users-seed-file.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const { faker } = require('@faker-js/faker')
+const bcrypt = require('bcryptjs')
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    const users = await Promise.all(
+      Array.from({ length: 20 }, async (_, index) => ({
+        name: `user${index + 1}`,
+        email: `user${index + 1}@example.com`,
+        password: await bcrypt.hash('12345678', 10),
+        avatar: faker.image.avatar(),
+        nation: faker.location.country(),
+        introduction: faker.lorem.sentence(),
+        created_at: new Date(),
+        updated_at: new Date()
+      }))
+    )
+    await queryInterface.bulkInsert('Users', users)
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('Users', null, {})
+  }
+}
