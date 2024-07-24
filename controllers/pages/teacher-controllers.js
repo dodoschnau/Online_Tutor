1e5a1e2c95ab5c48e92b5136c605bd32478f9cc1
dodoschnau <huangmeisin@gmail.com>
2024-07-24 14:06:41 +0800

    feat: add search bar

--- controllers/pages/teacher-controllers.js
+++ controllers/pages/teacher-controllers.js
@@ -1,8 +1,20 @@
+const { Op } = require('sequelize')
 const { User, Teacher } = require('../../models')
 
 const teacherControllers = {
   getTeachers: async (req, res) => {
+    const keyword = req.query.keyword?.trim().toLowerCase() || ''
+    const where = keyword
+      ? {
+          [Op.or]: [
+            { name: { [Op.like]: `%${keyword}%` } },
+            { nation: { [Op.like]: `%${keyword}%` } }
+          ]
+        }
+      : {}
+
     const users = await User.findAll({
+      where,
       include: [{
         model: Teacher,
         as: 'teacher',
