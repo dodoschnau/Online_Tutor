942dd9f52705e7c069c3a6302e671ffd69899448
dodoschnau <huangmeisin@gmail.com>
2024-07-25 14:37:45 +0800

    feat: add flash messages

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const { engine } = require('express-handlebars')
+const session = require('express-session')
+const flash = require('connect-flash')
 
 const app = express()
 const port = process.env.PORT || 3000
+const SESSION_SECRET = 'secret'
 
 const { pages } = require('./routes')
 const handlebarsHelpers = require('./helpers/handlebars-helpers')
+const messageHandler = require('./middlewares/message-handler')
 
 app.engine('.hbs', engine({
   extname: '.hbs',
@@ -15,6 +19,16 @@ app.set('view engine', '.hbs')
 app.set('views', './views')
 
 app.use(express.urlencoded({ extended: true }))
+
+app.use(session({
+  secret: SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(flash())
+
+app.use(messageHandler)
+
 app.use(pages)
 
 app.listen(port, () => {
--- controllers/pages/root-controllers.js
+++ controllers/pages/root-controllers.js
@@ -12,13 +12,16 @@ const rootControllers = {
     try {
       const { name, email, password, confirmPassword, nation } = req.body
       if (!name || !email || !nation) {
+        req.flash('error', 'Please fill in all fields.')
         return res.redirect('/register')
       }
       if (password !== confirmPassword) {
+        req.flash('error', 'Passwords do not match.')
         return res.redirect('/register')
       }
       const hashedPassword = await bcrypt.hash(password, 10)
       await User.create({ name, email, password: hashedPassword, nation })
+      req.flash('success', 'Account created successfully.')
       return res.redirect('/login')
     } catch (error) {
       console.log(error)
