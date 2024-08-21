0f178785a589e722bb5375db6e640e60128cccb9
dodoschnau <huangmeisin@gmail.com>
2024-08-21 14:06:00 +0800

    feat: add pagination on admin page

--- controllers/pages/admin-controllers.js
+++ controllers/pages/admin-controllers.js
@@ -1,14 +1,28 @@
 const { User } = require('../../models')
+const { getOffset, getPagination } = require('../../helpers/pagination-helper')
 
 const adminControllers = {
   getAdminDashboard: async (req, res, next) => {
     try {
-      const users = await User.findAll({
-        attributes: ['id', 'name', 'avatar', 'isTeacher'],
+      const DEFAULT_LIMIT = 10
+      const page = parseInt(req.query.page) || 1
+      const limit = parseInt(req.query.limit) || DEFAULT_LIMIT
+      const offset = getOffset(page, limit)
+
+      const { count, rows } = await User.findAndCountAll({
+        where: { isAdmin: false },
+        attributes: ['id', 'name', 'avatar', 'isTeacher', 'isAdmin'],
+        offset,
+        limit,
         raw: true
       })
 
-      return res.render('admin-dashboard', { users })
+      const pagination = getPagination(count, page, limit)
+
+      return res.render('admin-dashboard', {
+        users: rows,
+        pagination
+      })
     } catch (error) {
       next(error)
     }
